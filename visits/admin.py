4de15c0622ecfff970661890b248ba70fa7e788e
agent47daun <agent47daun@gmail.com>
2022-05-05 16:13:36 +0400

    Админка для моделей

--- visits/admin.py
+++ visits/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import TradePoint, Employee, Visit
+
+@admin.register(TradePoint)
+class TradePointAdmin(admin.ModelAdmin):
+    search_fields = ("title", )
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    search_fields = ("name", )
+
+@admin.register(Visit)
+class VisitAdmin(admin.ModelAdmin):
+    search_fields = ("tradepoint__title", "tradepoint__employee__name")
