ae2337e18859e686495913d8f9b2c99c705ecdcf
agent47daun <agent47daun@gmail.com>
2022-05-05 15:26:14 +0400

    Модели приложения

--- visits/models.py
+++ visits/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Employee(models.Model):
+    name = models.CharField(max_length=255)
+    phone = models.CharField(max_length=255)
+
+
+class TradePoint(models.Model):
+    title = models.CharField(max_length=255)
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name="tradepoints")
+
+
+class Visit(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    tradepoint = models.ForeignKey(TradePoint, on_delete=models.CASCADE, related_name="visits")
+    latitude = models.FloatField()
+    longitude = models.FloatField()
